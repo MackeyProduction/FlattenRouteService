940f153a1d3410383a2d86e0b36103079479e970
Til Anheier <til.anheier@check24.de>
2019-10-08 12:56:04 +0200

    adding defaults values from parent route to child routes.

--- src/Hydrator/FlattenRouteHydrator.php
+++ src/Hydrator/FlattenRouteHydrator.php
@@ -8,6 +8,7 @@
 
 namespace ZendFlattenRoute\Hydrator;
 
+use ZendFlattenRoute\Model\FlattenRoute;
 
 class FlattenRouteHydrator extends AbstractFlattenRouteHydrator implements HydratorInterface
 {
@@ -15,6 +16,18 @@ class FlattenRouteHydrator extends AbstractFlattenRouteHydrator implements Hydra
     public function hydrate(array $routes)
     {
         $routeStack = $this->getFlattenRouteStack($routes);
+
+        // adding default values from parent route in child routes
+        foreach ($routes['routes'] as $route) {
+            $defaults = $route['options']['defaults'];
+
+            array_map(function (FlattenRoute $r) use ($defaults) {
+                $rDefaults = $r->getOptions()['defaults'] ?? [];
+                $r->setOptions(array_merge($r->getOptions(), [
+                    'defaults' => $defaults + $rDefaults
+                ]));
+            }, $routeStack->getRoutes());
+        }
 
         return array_reverse($routeStack->getRoutes());
     }
